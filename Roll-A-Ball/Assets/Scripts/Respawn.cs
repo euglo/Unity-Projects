980a8eaa39088548ca8ced3834fc3434a887df5b
Eugene Lo <bleugene12@g.ucla.edu>
2018-11-02 15:04:30 -0700

    Add roll

---
+++ Roll-A-Ball/Assets/Scripts/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+
+    public Transform playerTransform;
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = playerTransform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.transform.position = startPosition;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+}
